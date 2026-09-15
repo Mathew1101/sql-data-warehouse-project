bcdef5c837091300707777e2850dcac3c51b246e
Mathew1101 <mathew1101k@gmail.com>
2026-09-15 12:50:28 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,43 @@
+/*
+===========================
+Create Database and Schemas
+===========================
+
+Script Purpose:
+  This script creates a new database named 'DataWarehoseProject' after checking if it already exists.
+  If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+  within the database: 'bronze', 'silver', 'gold'.
+
+WARNING:
+  Running this script will drop the entire 'DataWarehoseProject' databse if it exists.
+  ALL data in the database will be pernamently deleted - proceed with caution 
+  and ensure you have proper backups before running this script.
+*/
+
+USE MASTER;
+GO
+
+-- Drop and recreate the 'DataWarehoseProject' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehoseProject')
+BEGIN
+  ALTER DATABASE DataWarehoseProject SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehoseProject;
+END;
+GO
+
+-- Create the 'DataWarehoseProject' database  
+CREATE DATABASE DataWarehoseProject;
+GO
+  
+USE DataWarehouseProject;
+GO
+  
+--Create Schemas
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
