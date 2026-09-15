6c7bd1e0efd63bef0f5416de821b6725d5338346
Mathew1101 <mathew1101k@gmail.com>
2026-09-15 21:18:58 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -50,23 +50,23 @@ sls_price INT
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
-CID	VARCHAR (50),
-BDATE DATE,
-GEN VARCHAR (50)
+cid	VARCHAR (50),
+bdate DATE,
+gen VARCHAR (50)
 );
 
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
-CID	VARCHAR (50),
-CNTRY VARCHAR (50)
+cid	VARCHAR (50),
+cntry VARCHAR (50)
 );
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-ID VARCHAR (50),
-CAT VARCHAR (50),
-SUBCAT VARCHAR (50),
-MAINTENANCE VARCHAR (50)
+id VARCHAR (50),
+cat VARCHAR (50),
+subcat VARCHAR (50),
+maintenance VARCHAR (50)
 );
