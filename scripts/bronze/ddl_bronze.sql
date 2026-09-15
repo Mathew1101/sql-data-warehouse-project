61d5767e8731b21b2469bacb1e46554935233fc2
Mathew1101 <mathew1101k@gmail.com>
2026-09-15 21:45:40 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -20,6 +20,7 @@ cst_marital_status VARCHAR (50),
 cst_gndr VARCHAR (50),
 cst_create_date DATE
 );
+GO
 
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
@@ -32,6 +33,7 @@ prd_line VARCHAR (50),
 prd_start_dt DATE,
 prd_end_dt DATE
 );
+GO
 
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
@@ -46,6 +48,7 @@ sls_sales INT,
 sls_quantity INT,
 sls_price INT
 );
+GO
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
@@ -54,6 +57,7 @@ cid	VARCHAR (50),
 bdate DATE,
 gen VARCHAR (50)
 );
+GO
 
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
@@ -61,6 +65,7 @@ CREATE TABLE bronze.erp_loc_a101 (
 cid	VARCHAR (50),
 cntry VARCHAR (50)
 );
+GO
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
@@ -70,3 +75,4 @@ cat VARCHAR (50),
 subcat VARCHAR (50),
 maintenance VARCHAR (50)
 );
+GO
