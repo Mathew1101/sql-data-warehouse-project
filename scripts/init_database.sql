f50117ba554df64fc19ed99141c19fc1181d1632
Mathew1101 <mathew1101k@gmail.com>
2026-09-15 16:05:53 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,7 +1,7 @@
 /*
-===========================
+=======================================================================================================
 Create Database and Schemas
-===========================
+=======================================================================================================
 
 Script Purpose:
   This script creates a new database named 'DataWarehoseProject' after checking if it already exists.
@@ -12,6 +12,7 @@ WARNING:
   Running this script will drop the entire 'DataWarehoseProject' databse if it exists.
   ALL data in the database will be pernamently deleted - proceed with caution 
   and ensure you have proper backups before running this script.
+=======================================================================================================
 */
 
 USE MASTER;
